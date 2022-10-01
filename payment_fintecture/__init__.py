69b7469a280774d15968d5d21c8fbe4929805af4
Gabriel <teloconf@gmail.com>
2022-10-01 14:31:53 +0200

    [FIX]

--- payment_fintecture/__init__.py
+++ payment_fintecture/__init__.py
@@ -3,12 +3,12 @@ from . import models
 
 from odoo import api, SUPERUSER_ID
 
-from odoo.addons.payment import reset_payment_acquirer
+from odoo.addons.payment import reset_payment_provider
 from odoo.addons.payment_fintecture.const import PAYMENT_ACQUIRER_NAME
 
 
 def uninstall_hook(cr, registry):
-    reset_payment_acquirer(cr, registry, PAYMENT_ACQUIRER_NAME)
+    reset_payment_provider(cr, registry, PAYMENT_ACQUIRER_NAME)
 
 
 def post_init_hook(cr, registry):
